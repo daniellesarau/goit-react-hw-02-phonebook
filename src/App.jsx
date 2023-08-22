4f407f6a24371c52bdfdd84b477e93df6e323534
daniellesarau <123309317+daniellesarau@users.noreply.github.com>
2023-08-22 09:56:59 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ export const App = () => {
     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
   ]);
-  const [filter, SetFiler] = useState('');
+  const [filter, setFilter] = useState('');
 
   const onSubmitHandler = (name, number) => {
     const contact = {
@@ -31,7 +31,7 @@ export const App = () => {
 
   const handleChange = event => {
     const { value } = event.target;
-    SetFiler(value);
+    setFilter(value);
   };
 
   const handleDelete = id => {
